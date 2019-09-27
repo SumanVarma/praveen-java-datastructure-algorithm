3bc0bd7900ff0904172a660b62b9f7a248e40b0b
Praveen Oruganti <Praveen Oruganti@PraveenOruganti>
2019-09-27 18:30:55 +0530

    LinkedList DS

---
+++ src/main/java/com/praveen/dsalg/Sample.java
@@ -0,0 +1,15 @@
+package com.praveen.dsalg;
+
+import java.util.ArrayList;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		ArrayList l = new ArrayList();
+		l.add("Praveen");
+		l.add(1);
+		System.out.println(l);
+		
+	}
+
+}
