989c00cf12260eb412a0d25d41aa183b67b43e89
praveen <praveen@praveen-PC>
2019-10-26 19:25:19 +0530

    updated

--- src/main/java/com/praveen/dsalg/others/SwapTwoNumbers.java
+++ src/main/java/com/praveen/dsalg/others/SwapTwoNumbers.java
@@ -7,25 +7,25 @@ public class SwapTwoNumbers {
 	}
 
 	public static void swapUsingTemp(int num1, int num2) {
-		System.out.println(" swapUsingTemp Method Entry");
+		System.out.println("swapUsingTemp Method Entry");
 		int temp = 0;
 		System.out.println("Before Swapping :" + " num1 is " + num1 + " num2 is " + num2);
 		temp = num1;
 		num1 = num2;
 		num2 = temp;
 		System.out.println("After Swapping :" + " num1 is " + num1 + " num2 is " + num2);
-		System.out.println(" swapUsingTemp Method Exit");
+		System.out.println("swapUsingTemp Method Exit");
 
 	}
 
 	public static void swapWithoutTemp(int num1, int num2) {
-		System.out.println(" swapWithoutTemp Method Entry");
+		System.out.println("swapWithoutTemp Method Entry");
 		System.out.println("Before Swapping :" + " num1 is " + num1 + " num2 is " + num2);
 		num1 = num1 + num2;
 		num2 = num1 - num2;
 		num1 = num1 - num2;
 		System.out.println("After Swapping :" + " num1 is " + num1 + " num2 is " + num2);
-		System.out.println(" swapWithoutTemp Method Exit");
+		System.out.println("swapWithoutTemp Method Exit");
 
 	}
 }
